fabc09353a24851d8862db20af9c1965ed4d20d2
Coutinho <leon.coutinho1@gmail.com>
2022-01-17 23:17:11 -0300

    Adaptação para uso de diversas salas

--- index.js
+++ index.js
@@ -30,79 +30,78 @@ const roundPoints = (value) => {
     }   
 }
 
-const server = app.listen(process.env.PORT || 3000, () => {
-    console.log(buildLog(`server running on port ${process.env.PORT || 3000}`));
+const server = app.listen(process.env.PORT || 3001, () => {
+    console.log(buildLog(`server running on port ${process.env.PORT || 3001}`));
 });
 
 io = socket(server);
 
 let players = [];
-let reveal = false;
 
 io.on("connection", (socket) => {
     socket.on("join_room", (data) => {
-        socket.join(data)
+        socket.join(data.room);
+        /*if (room.findIndex(r => r.id == data.room) === -1) {
+            console.log(`cria sala ${data.room}`);
+        }*/
         players.push({
-            name: data,
+            name: data.user,
             id: socket.id,
+            room: data.room,
             vote: null
         });
 
-        io.emit("players", players);
-        console.log(buildLog(`User ${data} arrived at room`));
-        reveal = false;
-        io.emit("reveal", reveal);
-        io.emit('result', '');
+        io.to(data.room).emit("players", players.filter(p => p.room == data.room));
+        console.log(buildLog(`User ${data.user} arrived at room ${data.room}`));
     });
 
-    socket.on("vote", (value) => {
-        if (!reveal) {
+    socket.on("vote", (data) => {
+        if (players.filter(p => p.room == data.room).some(p => p.vote == null)) {
             players.map(p => {
                 if (p.id == socket.id) {
-                    p.vote = value;
+                    p.vote = data.value;
                 }
             });
             
-            io.emit("players", players);
+            io.to(data.room).emit("players", players.filter(p => p.room == data.room));
                         
-            if (!players.some(p => p.vote == null)) {
-                reveal = true;
+            if (!players.filter(p => p.room == data.room).some(p => p.vote == null)) {
+                io.to(data.room).emit('reveal', true);
                 let sum = 0;
-                players.forEach(p => sum += p.vote);
-                io.emit('result', roundPoints(sum / players.length));
-                io.emit("reveal", reveal);
+                players.filter(p => p.room == data.room).forEach(p => sum += p.vote);
+                io.to(data.room).emit('result', roundPoints(sum / players.filter(p => p.room == data.room).length));
             }
         }
     });
 
-    socket.on('reveal', () => {
+    socket.on('reveal', (data) => {
         let count = 0;
         let sum = 0;
-        players.forEach(p => {
+        players.filter(p => p.room == data.room).forEach(p => {
             if (p.vote != null) {
                 count++;
                 sum += p.vote;
             }
         });
         reveal = true;
-        io.emit('result', roundPoints(sum / count));
-        io.emit('reveal', reveal);
-    })
+        io.to(data.room).emit('result', roundPoints(sum / count));
+        io.to(data.room).emit('reveal', true);
+    });
 
-    socket.on('reset', () => {
-        players.map(p => p.vote = null);
-        reveal = false;
-        io.emit('players', players);
-        io.emit('reveal', reveal);
-        io.emit('result', '');
+    socket.on('reset', (data) => {
+        players.filter(p => p.room == data.room).map(p => p.vote = null);
+        
+        io.to(data.room).emit('players', players.filter(p => p.room == data.room));
+        io.to(data.room).emit('reveal', false);
+        io.to(data.room).emit('result', '');
     });
 
     socket.on("disconnect", () => {
-        playerDisconected = players.filter(p => p.id == socket.id);
-        if (playerDisconected.length > 0)
-            console.log(buildLog(`User ${playerDisconected[0].name} disconnected`));
-        players = players.filter(p => p.id != socket.id);
-        io.emit("players", players);
-        
-    })
+        playerDisconnected = players.filter(p => p.id == socket.id);
+        if (playerDisconnected.length > 0) {
+            console.log(buildLog(`User ${playerDisconnected[0].name} disconnected`));
+            players = players.filter(p => p.id != socket.id);
+            io.to(playerDisconnected[0].room).emit("players", players.filter(p => p.room == playerDisconnected[0].room));
+        }        
+    });
 });
